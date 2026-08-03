da83837e8d0d0dcd60d7726267357c5234d14741
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-03 20:32:18 +0530

    I modified it to my origin

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,5 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+       System.out.println("Welcome to Java");
     }
 }
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/today.java
@@ -0,0 +1,5 @@
+public class today {
+    public static void main(String[] args) {
+       System.out.println("Today is a good day to learn");
+    }
+}
