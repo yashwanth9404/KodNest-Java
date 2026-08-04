605d7b7daa86686bb1078eb97b24ad7b6236e935
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-04 12:25:12 +0530

    Checking Number

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/p2.java
@@ -0,0 +1,16 @@
+public class p2{
+  
+  public static void main(String[] args) {
+      int num = 25;
+    if (num > 0){
+      System.out.println("Positive");
+    }else if (num < 0){
+      System.out.println("Negative");
+    }else{
+      System.out.println("Zero");
+    }
+  }
+
+}
+  
+  
