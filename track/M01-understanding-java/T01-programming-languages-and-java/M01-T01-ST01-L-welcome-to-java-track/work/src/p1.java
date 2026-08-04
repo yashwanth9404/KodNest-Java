994f1996080f96a06ab526e4ec304e3aedb35a44
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-04 13:30:57 +0530

    Weekdays

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/p1.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/p1.java
@@ -3,7 +3,7 @@ public class p1{
   public static void main(String[] args) {
       
       System.out.println("Monday");
-      System.out.print("Tuesday");
+      System.out.println("Tuesday");
       System.out.println("Wednesday");
       System.out.println("Thursday");
       System.out.print("Friday");
