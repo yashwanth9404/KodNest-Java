b6be8ec54b906dc27f0ef9711db0ae63103e75eb
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-04 11:33:02 +0530

    Weekdays ouput

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/p1.java
@@ -0,0 +1,16 @@
+public class p1{
+  
+  public static void main(String[] args) {
+      
+      System.out.println("Monday");
+      System.out.print("Tuesday");
+      System.out.println("Wednesday");
+      System.out.println("Thursday");
+      System.out.print("Friday");
+      System.out.println("Saturday");
+      System.out.println("Sunday");
+
+  }
+
+
+}
