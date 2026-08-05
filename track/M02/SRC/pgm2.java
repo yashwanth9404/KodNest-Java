1883d186a14d877265fcac7fb6a180e2e0e8e62c
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-05 12:09:20 +0530

    Post increment

---
+++ track/M02/SRC/pgm2.java
@@ -0,0 +1,15 @@
+public class pgm2{
+
+public static void main(String[] args) {
+    
+
+int a = 10;
+
+int res = a++ + ++a + ++a + ++a + a++ + ++a;
+
+System.out.println(res);
+System.out.println(a);
+
+}
+
+}
