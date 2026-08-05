b788cea4a41e07df07c8cd169e1a4204383099d4
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-05 12:33:29 +0530

    pgm3 & pgm4

---
+++ track/M02/SRC/pgm3.java
@@ -0,0 +1,10 @@
+public class pgm3{
+
+    public static void main(String[] args){
+            
+        for(int i=0;i<=5;i++){
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ track/M02/SRC/pgm4.java
@@ -0,0 +1,12 @@
+public class pgm4{
+    public static void main(String[] args){
+
+        int i =1;
+        while(i<=5){
+
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+}
