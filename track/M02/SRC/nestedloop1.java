57833ce803186981d1b73c2772c5845fbcace2e0
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-06 12:29:58 +0530

    break

--- track/M02/SRC/nestedloop1.java
+++ track/M02/SRC/nestedloop1.java
@@ -3,12 +3,12 @@ public class nestedloop1{
 
         for (int i=1;i<=10;i++){
 
-            for(int j=1;j<=10;j++){
+            for(int j=i;j<=10;j++){
             
-                System.out.println(j);
+                System.out.print("*");
             
             }
-           System.out.println(); 
+           System.out.println();
         }
         
     }
