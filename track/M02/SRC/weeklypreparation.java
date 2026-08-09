8bf016f3bfa34bbca915b8ddb6b438eff1474fdf
Yashwanth <yashwanthpeddakudala@gmail.com>
2026-08-10 00:51:53 +0530

    Module 2

---
+++ track/M02/SRC/weeklypreparation.java
@@ -0,0 +1,15 @@
+public class weeklypreparation   {
+    public static void main(String[] args) {
+        int javaHoursPerDay = 2;
+        int aptitudeHoursPerDay = 1;
+        int numberOfDays = 5;
+
+        int weeklyJavaHours = javaHoursPerDay * numberOfDays;
+        int weeklyAptitudeHours = aptitudeHoursPerDay * numberOfDays;
+        int totalPreparationHours = weeklyJavaHours + weeklyAptitudeHours;
+
+        System.out.println("Java: " + weeklyJavaHours);
+        System.out.println("Aptitude: " + weeklyAptitudeHours);
+        System.out.println("Total: " + totalPreparationHours);
+    }
+}
